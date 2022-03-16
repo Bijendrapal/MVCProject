f57ff1ca42020a5786ef623dd5feebca8b19f7ec
Bijendra pal <99710759+Bijendrapal@users.noreply.github.com>
2022-03-16 18:37:52 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Projectmvc.Models;
+using Projectmvc.mymodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projectmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            chut1Entities obj = new chut1Entities();
+            var res  = obj.My_data;
+
+         
+
+            return View(res);
+        }
+       
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "viewbag.";
+            ViewData["msg"] = "ViewData";
+            TempData["msg1"] = "TempData";
+            Session["msg2"] = "Session";
+
+            return View();
+        }
+        
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            chut1Entities obj = new chut1Entities();
+            var res = obj.My_data;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create( My_data data)
+        {
+            chut1Entities entities = new chut1Entities();
+            My_data table = new My_data();
+            table.id = data.id;
+            table.name = data.name;
+            table.Email = data.Email;
+            table.salary = data.salary;
+            entities.My_data.Add(data);
+            entities.SaveChanges();
+
+
+            return View();
+        }
+    }
+}
---
+++ Models/student1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projectmvc.Models
+{
+    public class student1
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Email { get; set; }
+        public Nullable<int> salary { get; set; }
+    }
+}
